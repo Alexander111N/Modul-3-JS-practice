7064f1bdf7d49f7e3b58b722e9500898a1a3b231
Alexander <test123@yandex.ru>
2022-04-18 14:17:08 +0300

    timer

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ document.addEventListener('DomContentLoaded',() => {
     alert('загрузка прошла');
 });
 
+// Tabs
 const tabs = document.querySelectorAll('.tabheader__item');
 const tabsContent = document.querySelectorAll('.tabcontent');
 const tabsParent = document.querySelector('.tabheader__items');
@@ -43,26 +44,59 @@ tabsParent.addEventListener('click', (event) => {
     }
 });
 
-
-
 hideTabContent();
 showTabContent(0);
 
-// const timer = setTimeout(function(text){
-//     console.log(text);
-// }, 2000, 'Hello');
+// Timer
 
-// let count = 0;
-// const repeate = setInterval(function(){
-//     console.log('интервал');
-//     count++;
-//     if(count == 10){
-//         clearInterval(repeate);
-//     }
-// }, 1000);
+const deadline = '2022-04-19';
 
-const date = new Date();
-console.log(date);
-const date1 = new Date(2021, 3, 4, 5);
-console.log(date1);
-console.log(Date.now());
+function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+       days = Math.floor(t / (1000 * 60 * 60 * 24)),
+       hours = Math.floor(t / (1000 * 60 * 60) % 24 ),
+       minutes = Math.floor(t / (1000 * 60) % 60 ),
+       seconds = Math.floor((t / 1000) % 60 );
+
+    return {
+        total: t,
+        days: days,
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds
+    };
+
+}
+
+function addZero(num) {
+    if(num >= 0 && num < 10){
+        return `0${num}`;
+    }
+    else {
+        return num;
+    }
+}
+
+function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = timer.querySelector('#seconds');
+
+    updateClock();
+    const interval = setInterval(updateClock, 1000);     
+
+    function updateClock(){
+        const t = getTimeRemaining(endtime);
+        days.textContent = addZero(t.days);
+        hours.textContent = addZero(t.hours);
+        minutes.textContent = addZero(t.minutes);
+        seconds.textContent = addZero(t.seconds);
+        if(t.total <= 0){
+            clearInterval(interval);
+        }
+    }
+}
+
+setClock('.timer', deadline);
